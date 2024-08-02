a1e0cc0d31139a8adfdf77e2449a7ba37618bea1
MyselfRoshan <roshanghimire699@protonmail.com>
2024-08-02 12:49:48 +0545

    removed conf folder, Dockerfile for simplification

--- src/main/java/DemoServlet.java
+++ src/main/java/DemoServlet.java
@@ -1,26 +1,27 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.servlet.Servlet;
+import jakarta.servlet.http.HttpServlet;
 
-public class DemoServlet implements Servlet {
-    public String getServletInfo() {
-        return null;
-    };
+public class DemoServlet extends HttpServlet {
+    // public String getServletInfo() {
+    // return null;
+    // };
 
-    public jakarta.servlet.ServletConfig getServletConfig() {
-        return null;
-    };
+    // public jakarta.servlet.ServletConfig getServletConfig() {
+    // return null;
+    // };
 
-    public void init(jakarta.servlet.ServletConfig config) throws jakarta.servlet.ServletException {
-    };
+    // public void init(jakarta.servlet.ServletConfig config) throws
+    // jakarta.servlet.ServletException {
+    // };
 
     public void service(jakarta.servlet.ServletRequest req, jakarta.servlet.ServletResponse res)
             throws jakarta.servlet.ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.print("Hello from the server sdfsdg");
+        out.print("Hello from the demo Servelet");
     };
 
-    public void destroy() {
-    };
+    // public void destroy() {
+    // };
 }
